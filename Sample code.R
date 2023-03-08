98565fc58fffc83034cb3fc41038cac0c3acce7f
Yucheng Liu <marsxxxx@uw.edu>
2023-03-08 15:29:12 +0800

    Completing CPI Graphs

--- Sample code.R
+++
@@ -1,6 +0,0 @@
-# Sample Code to import data
-library(readxl)
-CPIForecastData <- read_excel("CPIForecast.xlsx",
-                              skip = 1)
-CPIHistoricalData <- read_excel("historicalcpi.xlsx", 
-                            skip = 1)
